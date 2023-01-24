669629cd530a294dbd34522c030d440bfd8a4572
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2023-01-24 12:25:58 +0000

    Init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+
+const App = () => <div>Hello, World!</div>;
+const app = document.getElementById(`app`);
+const root = createRoot(app);
+root.render(<App />);
