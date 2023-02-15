710f5496a6ed478945e7ff6d4f08379608291d22
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2023-02-15 20:05:10 +0000

    Reply button

--- src/web/Answer.tsx
+++ src/web/Answer.tsx
@@ -38,7 +38,7 @@ const Answer: React.FC<AnswerProps> = ({elem, lang, onReplyClick}) => {
         TransitionProps={{timeout: 600}}>
         <Stack direction="row" alignItems="center" sx={{position: "absolute", bottom: 5, right: 5}}>
           <IconButton disabled={elem.getReplyClicked()} onClick={() => onReplyClick(elem)}>
-            <Reply color={"success"}/>
+            <Reply color={elem.getReplyClicked() ? "inherit" : "success"}/>
           </IconButton>
           <IconButton onPointerDown={handleCopy}>
             <ContentCopy/>
