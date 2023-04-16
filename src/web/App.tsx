4e92a45872bccc1ba23eeb7538434b07964ca6d5
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2023-04-16 18:39:23 +0100

    Add mono

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,9 +1,10 @@
-import './styles.css';
+import "./styles.css";
 import * as React from "react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import {
   Alert,
   Box,
+  Button,
   Collapse,
   Container,
   createTheme,
@@ -14,21 +15,20 @@ import {
   Link,
   Stack,
   ThemeProvider,
-  Typography,
   useMediaQuery,
 } from "@mui/material";
-import {ExpandMore, GitHub, Instagram, Twitter,} from "@mui/icons-material";
-import {useTranslation} from "react-i18next";
+import { ExpandMore, GitHub, Instagram, Twitter } from "@mui/icons-material";
+import { useTranslation } from "react-i18next";
 import Answer from "./Answer";
 import Prompt from "./Prompt";
-import {ConversationElem, PromptElem} from "./Types";
-import {FundingBar} from "./FundingBar";
+import { ConversationElem, type PromptElem } from "./Types";
+import { FundingBar } from "./FundingBar";
 
 export default function App(): JSX.Element {
-  const {t, i18n} = useTranslation("translation");
+  const { t, i18n } = useTranslation("translation");
   const mode = useMediaQuery(`(prefers-color-scheme: dark)`);
   const theme = React.useMemo(
-    () => createTheme({palette: {mode: mode ? `dark` : `light`}}),
+    () => createTheme({ palette: { mode: mode ? `dark` : `light` } }),
     [mode]
   );
 
@@ -37,12 +37,12 @@ export default function App(): JSX.Element {
   const [moneyLeft, setMoneyLeft] = useState<number | null>(null);
   const [donatedAmount, setDonatedAmount] = useState(0);
   const [lastRequestCost, setLastRequestCost] = useState(0);
-  const [conversation, setConversation] = useState<ConversationElem[]>(conversationLoader);
+  const [conversation, setConversation] =
+    useState<ConversationElem[]>(conversationLoader);
   useEffect(() => {
     try {
-      localStorage.setItem('gpt_conversation', JSON.stringify(conversation));
-    } catch (e) {
-    }
+      localStorage.setItem("gpt_conversation", JSON.stringify(conversation));
+    } catch (e) {}
   }, [conversation]);
 
   const lang = i18n.language;
@@ -57,11 +57,92 @@ export default function App(): JSX.Element {
     changeLanguage(browserLang);
   }, []);
 
+  function sendConversation() {
+    const messages = buildMessaages(conversation, lang);
+
+    setLoading(true);
+
+    // @ts-expect-error
+    grecaptcha.enterprise
+      .execute("6LemuPokAAAAAGa_RpQfdiCHbbaolQ1i3g-EvNom", { action: "login" })
+      .then(function (token) {
+        const serviceURL =
+          "https://2g5qt6esgqbgc6cuvkfp7kgq4m0ugzcm.lambda-url.eu-west-3.on.aws";
+        fetch(serviceURL, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            messages,
+            v: 3,
+            da: donatedAmount || undefined,
+            token,
+          }),
+        })
+          .then(async (response) => {
+            setDonatedAmount(0);
+            return await response.text().then((text) => {
+              try {
+                return JSON.parse(text);
+              } catch (e) {
+                throw new Error(text);
+              }
+            });
+          })
+          .then((gptReply) => {
+            console.log(gptReply);
+            if (gptReply?.error) {
+              setError(gptReply?.answer || gptReply?.error);
+            } else {
+              const answer = gptReply?.answer ?? "";
+              const originalAnswer =
+                gptReply?.choices?.[0]?.message.content.trim() ?? "";
+              const lastPrompt = conversation
+                .filter((elem) => elem.isUser)
+                .pop();
+              if (lastPrompt) {
+                lastPrompt.answered = true;
+              }
+              setConversation((conversation: ConversationElem[]) => {
+                const newConv = [
+                  ...conversation,
+                  ConversationElem.newAnswer(
+                    conversation.length,
+                    answer,
+                    originalAnswer
+                  ),
+                  ConversationElem.newPrompt(conversation.length + 1, ""),
+                ];
+                // Mark dropped messages
+                const lastDroppedMessageId =
+                  gptReply.lastDroppedMessageId ?? -1;
+                if (lastDroppedMessageId >= 0) {
+                  newConv.forEach((el) => {
+                    el.dropped = el.id <= lastDroppedMessageId;
+                  });
+                }
+                return newConv;
+              });
+            }
+            if (isFinite(+gptReply?.moneyLeft)) {
+              setMoneyLeft(+gptReply?.moneyLeft);
+              setLastRequestCost(+gptReply?.cost || 0);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            setError(err?.message);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      });
+  }
+
   const handleSend = (el: PromptElem) => {
     setError("");
 
     if (el.getText().length <= 1) {
-      setError(t('errors.notEnoughDetails'));
+      setError(t("errors.notEnoughDetails"));
       return;
     }
 
@@ -72,67 +153,7 @@ export default function App(): JSX.Element {
       setConversation(conversation);
     }
 
-    const messages = buildMessaages(conversation, lang);
-
-    setLoading(true);
-
-    // @ts-ignore
-    grecaptcha.enterprise.execute('6LemuPokAAAAAGa_RpQfdiCHbbaolQ1i3g-EvNom', {action: 'login'}).then(function (token) {
-      const serviceURL = "https://2g5qt6esgqbgc6cuvkfp7kgq4m0ugzcm.lambda-url.eu-west-3.on.aws"
-      fetch(serviceURL, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({messages, v: 2, da: donatedAmount || undefined, token}),
-      })
-        .then((response) => {
-          setDonatedAmount(0);
-          return response.text().then((text) => {
-            try {
-              return JSON.parse(text);
-            } catch (e) {
-              throw new Error(text);
-            }
-          });
-        })
-        .then((gptReply) => {
-          console.log(gptReply);
-          if (gptReply?.error) {
-            setError(gptReply?.answer || gptReply?.error);
-          } else {
-            const answer = gptReply?.answer ?? "";
-            const originalAnswer = gptReply?.choices?.[0]?.message.content.trim() ?? "";
-            const lastPrompt = conversation.filter((elem) => elem.isUser).pop();
-            if (lastPrompt) {
-              lastPrompt.answered = true;
-            }
-            setConversation((conversation: ConversationElem[]) => {
-
-              const newConv = [
-                ...conversation,
-                ConversationElem.newAnswer(conversation.length, answer, originalAnswer),
-                ConversationElem.newPrompt(conversation.length + 1, ""),
-              ];
-              // Mark dropped messages
-              const lastDroppedMessageId = gptReply.lastDroppedMessageId ?? -1;
-              if (lastDroppedMessageId >= 0) {
-                newConv.forEach(el => {
-                  el.dropped = el.id <= lastDroppedMessageId
-                })
-              }
-              return newConv
-            });
-          }
-          if (isFinite(+(gptReply?.moneyLeft))) {
-            setMoneyLeft(+gptReply?.moneyLeft);
-            setLastRequestCost(+gptReply?.cost || 0);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          setError(err?.message);
-        })
-        .finally(() => setLoading(false));
-    });
+    sendConversation();
   };
 
   const [expanded, setExpanded] = useState(false);
@@ -143,44 +164,84 @@ export default function App(): JSX.Element {
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <Container maxWidth="sm" sx={{padding: 2}}>
+      <CssBaseline />
+      <Container maxWidth="sm" sx={{ padding: 2 }}>
         <Stack spacing={2}>
           {/* center aligned GPT-UA */}
-          <Box sx={{padding: 2, textAlign: "center", position: "relative"}}>
+          <Box sx={{ padding: 2, textAlign: "center", position: "relative" }}>
             <h1>GPT-UA</h1>
-            <p style={{marginLeft: "105px", marginTop: "-38px", marginBottom: 0, padding: 0}}>chat</p>
+            <p
+              style={{
+                marginLeft: "105px",
+                marginTop: "-38px",
+                marginBottom: 0,
+                padding: 0,
+              }}
+            >
+              chat
+            </p>
           </Box>
           {conversation.map((elem, i) => {
-            return (
-              elem.isUser ?
-                <Prompt
-                  key={`${i}_${elem.text.length}`}
-                  elem={elem}
-                  onClickSend={handleSend}
-                  onClear={() => {
-                    setConversation([ConversationElem.newPrompt(0, "")]);
-                  }}
-                  showClear={conversation.length > 1}
-                  sendDisabled={loading}
-                /> :
-                <Answer
-                  key={i}
-                  elem={elem}
-                />
+            return elem.isUser ? (
+              <Prompt
+                key={`${i}_${elem.text.length}`}
+                elem={elem}
+                onClickSend={handleSend}
+                onClear={() => {
+                  setConversation([ConversationElem.newPrompt(0, "")]);
+                }}
+                showClear={conversation.length > 1}
+                sendDisabled={loading}
+              />
+            ) : (
+              <Answer key={i} elem={elem} />
             );
           })}
-          {loading && <LinearProgress/>}
+          {loading && <LinearProgress />}
           {error && <Alert severity="error">{error}</Alert>}
           {moneyLeft !== null && isFinite(moneyLeft) && (
-            <Alert sx={{'& .MuiAlert-message': {width: '100%'}}} icon={false}
-                   severity={moneyLeft <= 1 ? "warning" : "info"}>
-              <FundingBar target={120} value={moneyLeft} onDonated={(amount) => {
-                setDonatedAmount(amount)
-              }}></FundingBar>
+            <Alert
+              sx={{ "& .MuiAlert-message": { width: "100%" } }}
+              icon={false}
+              severity={moneyLeft <= 1 ? "warning" : "info"}
+            >
+              <FundingBar
+                target={120}
+                value={moneyLeft}
+                onDonated={(amount) => {
+                  setDonatedAmount(amount);
+                  sendConversation();
+                }}
+              ></FundingBar>
+              <Button
+                variant="contained"
+                color="primary"
+                size={"small"}
+                onClick={() =>
+                  window.open(
+                    `https://send.monobank.ua/jar/3Q3K3VdHuU`,
+                    "_blank"
+                  )
+                }
+                sx={{
+                  backgroundColor: "black",
+                  borderRadius: `8px`,
+                  mt: "16px",
+                  display: `flex`,
+                  mr: `auto`,
+                  ml: `auto`,
+                }}
+              >
+                Donate with Monobank
+              </Button>
             </Alert>
           )}
-          <Grid container justifyContent="center" spacing={2} paddingRight={"30px"}>
+          <Grid
+            container
+            justifyContent="center"
+            spacing={2}
+            paddingRight={"30px"}
+          >
             <Grid item>
               <Link href="https://github.com/bogdantimes" target="_blank">
                 <GitHub className="github-icon" />
@@ -192,30 +253,40 @@ export default function App(): JSX.Element {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="https://www.instagram.com/bogdantimes" target="_blank">
+              <Link
+                href="https://www.instagram.com/bogdantimes"
+                target="_blank"
+              >
                 <Instagram className="instagram-icon" />
               </Link>
             </Grid>
           </Grid>
           {/*  Link to check OpenAI service status */}
-          <Box sx={{textAlign: "center"}}>
-            <span style={{fontSize: "12px"}}>{t(`openai.slowResponse`)}</span> <Link href="https://status.openai.com/"
-                                                                                     target="_blank"
-                                                                                     style={{fontSize: "12px"}}>
-            OpenAI status
-          </Link>
-            <Box sx={{display: "block", marginLeft: "auto", marginRight: "auto"}}>
-              <span style={{fontSize: "12px"}}>Dashboard</span>
+          <Box sx={{ textAlign: "center" }}>
+            <span style={{ fontSize: "12px" }}>{t(`openai.slowResponse`)}</span>{" "}
+            <Link
+              href="https://status.openai.com/"
+              target="_blank"
+              style={{ fontSize: "12px" }}
+            >
+              OpenAI status
+            </Link>
+            <Box
+              sx={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+            >
+              <span style={{ fontSize: "12px" }}>Dashboard</span>
               <IconButton size={"small"} onClick={handleExpandClick}>
-                <ExpandMore sx={{
-                  transform: expanded ? "rotate(180deg)" : "none",
-                  transition: theme.transitions.create("transform", {
-                    duration: theme.transitions.duration.shortest,
-                  })
-                }}/>
+                <ExpandMore
+                  sx={{
+                    transform: expanded ? "rotate(180deg)" : "none",
+                    transition: theme.transitions.create("transform", {
+                      duration: theme.transitions.duration.shortest,
+                    }),
+                  }}
+                />
               </IconButton>
             </Box>
-            <Box sx={{position: "relative", pb: "130%", scale: "0.5"}}>
+            <Box sx={{ position: "relative", pb: "130%", scale: "0.5" }}>
               <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <iframe
                   style={{
@@ -223,9 +294,10 @@ export default function App(): JSX.Element {
                     top: "-50%",
                     width: "200%",
                     height: "100%",
-                    left: "-50%"
+                    left: "-50%",
                   }}
-                  src="https://cloudwatch.amazonaws.com/dashboard.html?theme=light&dashboard=GPT-UA_Dashboard&context=eyJSIjoidXMtZWFzdC0xIiwiRCI6ImN3LWRiLTIwNjgxMTU4MDM2NSIsIlUiOiJ1cy1lYXN0LTFfSUlPV3l6WGc0IiwiQyI6IjRldGQ2cTZtNHZqYzZidGRldGprYjdnNXBjIiwiSSI6InVzLWVhc3QtMTpjMDVkYTA4ZS0yMDdjLTQ0YTAtOWY0OC00Yzk1MWM5OTk5YTIiLCJNIjoiUHVibGljIn0="></iframe>
+                  src="https://cloudwatch.amazonaws.com/dashboard.html?theme=light&dashboard=GPT-UA_Dashboard&context=eyJSIjoidXMtZWFzdC0xIiwiRCI6ImN3LWRiLTIwNjgxMTU4MDM2NSIsIlUiOiJ1cy1lYXN0LTFfSUlPV3l6WGc0IiwiQyI6IjRldGQ2cTZtNHZqYzZidGRldGprYjdnNXBjIiwiSSI6InVzLWVhc3QtMTpjMDVkYTA4ZS0yMDdjLTQ0YTAtOWY0OC00Yzk1MWM5OTk5YTIiLCJNIjoiUHVibGljIn0="
+                ></iframe>
               </Collapse>
             </Box>
           </Box>
@@ -242,10 +314,13 @@ interface Message {
   isUser: boolean;
 }
 
-function buildMessaages(conversation: ConversationElem[], lang: string): Message[] {
+function buildMessaages(
+  conversation: ConversationElem[],
+  lang: string
+): Message[] {
   const messages: Message[] = [];
   for (const elem of conversation) {
-    if (elem.dropped) continue
+    if (elem.dropped) continue;
     messages.push({
       lang,
       id: elem.getId(),
@@ -258,16 +333,16 @@ function buildMessaages(conversation: ConversationElem[], lang: string): Message
 
 const conversationLoader = () => {
   try {
-    const c = localStorage.getItem('gpt_conversation');
+    const c = localStorage.getItem("gpt_conversation");
     if (c) {
       const convArr = JSON.parse(c) as ConversationElem[];
-      return convArr.map(e => {
-        return Object.assign(new ConversationElem(), e, {staticMode: true});
-      })
+      return convArr.map((e) => {
+        return Object.assign(new ConversationElem(), e, { staticMode: true });
+      });
     } else {
-      return [ConversationElem.newPrompt(0, "")]
+      return [ConversationElem.newPrompt(0, "")];
     }
   } catch (e) {
-    return [ConversationElem.newPrompt(0, "")]
+    return [ConversationElem.newPrompt(0, "")];
   }
 };
