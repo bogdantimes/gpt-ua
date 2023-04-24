e63227bd523a57eb47fd383ad91fd28d971e732d
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2023-04-24 10:43:47 +0100

    Update UAH - USD hint

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -240,7 +240,7 @@ export default function App(): JSX.Element {
                     {t(`budget.mono`)} (Google Pay / Visa / Mastercard)
                   </Button>
                   <Typography ml={"auto"} mr={"auto"} variant={"caption"}>
-                    100₴ = $2.7
+                    10₴ ≈ $0.26
                   </Typography>
                 </>
               </FundingBar>
