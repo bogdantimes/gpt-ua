9f7f2df104600358d6374b47de39be1cac9fa5dd
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2023-10-03 10:33:49 +0100

    Cyber theme updates

--- src/web/theme.ts
+++ src/web/theme.ts
@@ -5,13 +5,17 @@ const cyberpunkTheme = createTheme({
     mode: "dark",
     primary: {
       main: "#00ffff",
+      light: "#33ffff",
+      dark: "#00cccc",
     },
     secondary: {
       main: "#ff00ff",
+      light: "#ff33ff",
+      dark: "#cc00cc",
     },
     background: {
-      default: "#2a2a2a", // Adjust this value for a slightly lighter background
-      paper: "#333",
+      default: "#1a1a1a",
+      paper: "#222",
     },
     text: {
       primary: "#00ffff",
@@ -19,13 +23,13 @@ const cyberpunkTheme = createTheme({
     },
   },
   typography: {
-    fontFamily: "'Orbitron', sans-serif",
+    fontFamily: "Tomorrow, sans-serif",
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: `
         body {
-          background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("https://wallpaperaccess.com/download/cyberpunk-style-5127290");
+          background-image: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url("https://pbs.twimg.com/media/Ft2dHReWYAAr3qq?format=jpg&name=4096x4096");
           background-repeat: no-repeat;
           background-size: cover;
           background-position: center;
@@ -33,10 +37,24 @@ const cyberpunkTheme = createTheme({
         }
       `,
     },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          border: "1px solid #00ffff",
+          transition: "transform 0.2s, box-shadow 0.2s",
+          "&:hover": {
+            transform: "scale(1.05)",
+            boxShadow: "0 0 10px #00ffff, 0 0 20px #00ffff, 0 0 30px #00ffff",
+          },
+        },
+      },
+    },
     MuiCard: {
       styleOverrides: {
         root: {
           backgroundColor: "transparent",
+          border: "1px solid #00ffff",
+          boxShadow: "0 0 10px #00ffff",
         },
       },
     },
