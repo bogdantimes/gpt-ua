8708fece1a484c1f31b2ad0e19d67dd8d2cd47b0
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2024-05-04 14:33:04 +0100

    Attach PDFs

--- src/web/PromptVision.tsx
+++ src/web/PromptVision.tsx
@@ -6,20 +6,23 @@ import {
   styled,
   TextField,
 } from "@mui/material";
-import { AddAPhoto, Cancel, Clear, Send } from "@mui/icons-material";
+import { AttachFile, Cancel, Clear, Send } from "@mui/icons-material";
 import { type PromptElem } from "./Types";
 import { t } from "i18next";
+import { pdfjs } from "react-pdf"; // Import for PDF.js
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 // Styling the Paper component with MUI's styled API
 const ImagePreview = styled("img")(({ theme }) => ({
   position: "absolute",
-  left: -5, // Adjust for exact positioning
+  left: -5,
   top: "50%",
   transform: "translateY(-50%)",
-  width: 40, // Set the width to 50px
-  height: 40, // Set the height to 50px
+  width: 40,
+  height: 40,
   objectFit: "cover",
-  borderRadius: theme.shape.borderRadius * 2, // To match the TextField's border radius
+  borderRadius: theme.shape.borderRadius * 2,
   boxShadow: theme.shadows[3],
 }));
 
@@ -38,7 +41,7 @@ const DeleteButton = styled(IconButton)(({ theme }) => ({
   borderRadius: "50%",
 }));
 
-const UploadIcon = styled(AddAPhoto)(({ theme }) => ({
+const UploadIcon = styled(AttachFile)(({ theme }) => ({
   position: "absolute",
   left: 2,
   top: "50%",
@@ -69,52 +72,25 @@ const PromptVision: React.FC<PromptProps> = ({
   const label = isStartPrompt ? t("conversation.start") : t("conversation.you");
 
   const handlePaste = (items: DataTransferItemList | File[]) => {
+    const MAX_FILES = 5;
+    let processedFiles = 0;
+
     for (const item of items) {
+      if (processedFiles >= MAX_FILES) break; // Limit the number of files
       const file = item instanceof File ? item : item.getAsFile();
-      console.log(file);
-      if (file && /^image\/(png|jpeg|webp|gif)$/.test(file.type)) {
-        const reader = new FileReader();
-        reader.onload = (e: ProgressEvent<FileReader>) => {
-          const img = new Image();
-          img.onload = () => {
-            let scaleDownFactor = 1;
-            const resizeAndCheck = () => {
-              const targetWidth = img.width * scaleDownFactor;
-              const targetHeight = img.height * scaleDownFactor;
+      if (!file) continue;
 
-              // Create a canvas to draw the scaled image
-              const canvas = document.createElement("canvas");
-              canvas.width = targetWidth;
-              canvas.height = targetHeight;
-              const ctx = canvas.getContext("2d");
-              ctx?.drawImage(img, 0, 0, targetWidth, targetHeight);
-
-              // Convert the canvas to a data URL and check size
-              const imageDataURL = canvas.toDataURL("image/jpeg");
-              const byteString = atob(imageDataURL.split(",")[1]);
-              const byteStringLength = byteString.length;
-              console.log("SIZE", byteStringLength);
-              if (byteStringLength > 3145728) {
-                // More than 3 MB
-                scaleDownFactor *= 0.75; // Reduce size by 25%
-                console.log("SIZE REDUCED 10%");
-                resizeAndCheck(); // Recursive call
-              } else {
-                setImageSrc(imageDataURL);
-              }
-            };
-            resizeAndCheck();
-          };
-          // Set the source of the image to the FileReader result
-          img.src = e.target?.result as string;
-        };
-        reader.readAsDataURL(file);
+      if (/^image\/(png|jpeg|webp|gif)$/.test(file.type)) {
+        handleImage(setImageSrc, file);
+      } else if (file.type === "application/pdf") {
+        handlePDF(setText, file);
       }
+      processedFiles++;
     }
   };
 
   const handleDeleteImage = () => {
-    setImageSrc(""); // This will remove the image source and effectively delete the image preview
+    setImageSrc("");
   };
 
   return (
@@ -165,7 +141,8 @@ const PromptVision: React.FC<PromptProps> = ({
                 {!visionDisabled && !imageSrc && !isAnsweredReply && (
                   <Box sx={{ mt: "-4px" }}>
                     <input
-                      accept="image/png,image/jpeg,image/webp,image/gif"
+                      accept="image/png,image/jpeg,image/webp,image/gif,application/pdf"
+                      multiple
                       style={{ display: "none" }}
                       id="raised-button-file"
                       type="file"
@@ -217,3 +194,74 @@ const PromptVision: React.FC<PromptProps> = ({
 };
 
 export default PromptVision;
+
+function handleImage(
+  setImageSrc: (value: string | ((prevState: string) => string)) => void,
+  file,
+) {
+  const reader = new FileReader();
+  reader.onload = (e: ProgressEvent<FileReader>) => {
+    const img = new Image();
+    img.onload = () => {
+      let scaleDownFactor = 1;
+      const resizeAndCheck = () => {
+        const targetWidth = img.width * scaleDownFactor;
+        const targetHeight = img.height * scaleDownFactor;
+
+        // Create a canvas to draw the scaled image
+        const canvas = document.createElement("canvas");
+        canvas.width = targetWidth;
+        canvas.height = targetHeight;
+        const ctx = canvas.getContext("2d");
+        ctx?.drawImage(img, 0, 0, targetWidth, targetHeight);
+
+        // Convert the canvas to a data URL and check size
+        const imageDataURL = canvas.toDataURL("image/jpeg");
+        const byteString = atob(imageDataURL.split(",")[1]);
+        const byteStringLength = byteString.length;
+        console.log("SIZE", byteStringLength);
+        if (byteStringLength > 3145728) {
+          // More than 3 MB
+          scaleDownFactor *= 0.75; // Reduce size by 25%
+          console.log("SIZE REDUCED 10%");
+          resizeAndCheck(); // Recursive call
+        } else {
+          setImageSrc(imageDataURL);
+        }
+      };
+      resizeAndCheck();
+    };
+    // Set the source of the image to the FileReader result
+    img.src = e.target?.result as string;
+  };
+  reader.readAsDataURL(file);
+}
+
+function handlePDF(
+  setText: (value: string | ((prevState: string) => string)) => void,
+  file,
+) {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const typedArray = new Uint8Array(e.target.result);
+    pdfjs.getDocument({ data: typedArray }).promise.then((pdf) => {
+      let texts = "";
+      for (let num = 1; num <= pdf.numPages; num++) {
+        pdf.getPage(num).then((page) => {
+          page.getTextContent().then((textContent) => {
+            textContent.items.forEach((item) => (texts += item.str + " "));
+            if (num === pdf.numPages) {
+              setText((prev) => `${prev}
+
+${file.name.toUpperCase()}:
+\`\`\`
+${texts}
+\`\`\``.trim());
+            }
+          });
+        });
+      }
+    });
+  };
+  reader.readAsArrayBuffer(file);
+}
